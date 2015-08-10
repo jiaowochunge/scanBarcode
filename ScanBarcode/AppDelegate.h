8b2b947c6e6363af4b83bee98da25d97258ae215
taolv <taolv@taolvdeMac-mini.local>
2015-08-10 16:46:19 +0800

    Initial Commit

---
+++ ScanBarcode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScanBarcode
+//
+//  Created by taolv on 15/8/10.
+//  Copyright (c) 2015年 taolv365.ios.recycle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ScanBarcode/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ScanBarcode
+//
+//  Created by taolv on 15/8/10.
+//  Copyright (c) 2015年 taolv365.ios.recycle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
